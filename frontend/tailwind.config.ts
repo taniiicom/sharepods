fc01b3e2e2949d323494282d5a0705a1c093fa04
kazuki1023 <k.keio12569@gmail.com>
2024-10-26 16:17:58 +0900

    animationが上下どちらからもでるようにした

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,18 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       keyframes: {
-        'wave-anime': {
+        'wave-up': {
           'from': { transform: 'translateY(0) rotate(0deg)' },
           'to': { transform: 'translateY(-50%) rotate(360deg)' },
         },
-        // 'wave-move': {
-        //   'from': { transform: 'translateY(0%) rotate(0deg)' },
-        //   'to': { transform: 'translateY(-50%) rotate(360deg)' },
-        // },
+        'wave-down': {
+          'from': { transform: 'translateY(-100%) rotate(0deg)' },
+          'to': { transform: 'translateY(-50%) rotate(360deg)' },
+        },
       },
       animation: {
-        'wave-anime': 'wave-anime 8s linear forwards',
-        'wave-move': 'wave-move 8s forwards linear',
+        'wave-up': 'wave-up 8s linear forwards',
+        'wave-down': 'wave-down 8s forwards linear',
       },
       boxShadow: {
         'custom': '0 4px 20px rgba(0, 0, 0, 0.2)',
