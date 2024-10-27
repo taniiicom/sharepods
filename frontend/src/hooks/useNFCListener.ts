c522f39633f7973365ee67590cdad1ef3bf44ebb
Hiroki Ando <hikuohiku@gmail.com>
2024-10-27 10:37:05 +0900

    rm debug log

--- frontend/src/hooks/useNFCListener.ts
+++ frontend/src/hooks/useNFCListener.ts
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import WatchParty from "@/types/WatchParty";
 import GeographicalLocation from "@/types/GeographicalLocation";
+import WatchParty from "@/types/WatchParty";
+import { useEffect, useState } from "react";
 
 const useNFCListener = ({ latitude, longitude }: GeographicalLocation) => {
   const [nfcSupported, setNfcSupported] = useState(false);
@@ -22,8 +22,6 @@ const useNFCListener = ({ latitude, longitude }: GeographicalLocation) => {
       const nfcReader = new window.NDEFReader();
       await nfcReader.scan();
       nfcReader.onreadingerror = async () => {
-        console.log(latitude);
-        console.log(longitude);
         const res = await fetch(
           `https://api.sharepods.p1ass.com/watchparty?lat=${latitude}&lon=${longitude}`,
         );
